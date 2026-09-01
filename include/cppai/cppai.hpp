b120041c457951d56d323a9ee085bc929519a0d7
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-01 21:39:16 +0200

    feat: add public cppai umbrella header

---
+++ include/cppai/cppai.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <cppai/core/config.hpp>
+#include <cppai/core/error.hpp>
+#include <cppai/core/types.hpp>
+
+#include <cppai/tensor/tensor_shape.hpp>
+#include <cppai/tensor/tensor_storage.hpp>
