b0246e8f1dd0d81d6ca6f323cee1fd891f4557e6
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-01 22:21:41 +0200

    test(tensor): add tensor operation tests

--- tests/tensor/tensor_tests.cpp
+++ tests/tensor/tensor_tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 
+#include <cppai/tensor/tensor_operations.hpp>
 #include <cppai/tensor/tensor.hpp>
 #include <cppai/tensor/tensor_shape.hpp>
 #include <cppai/tensor/tensor_storage.hpp>
@@ -50,4 +51,75 @@ TEST(TensorTest, Ones)
     {
         EXPECT_EQ(tensor[i], 1.0);
     }
+}
+TEST(TensorOperationsTest, Add)
+{
+    cppai::Tensor lhs(
+        {2, 2},
+        {1.0, 2.0, 3.0, 4.0});
+
+    cppai::Tensor rhs(
+        {2, 2},
+        {5.0, 6.0, 7.0, 8.0});
+
+    cppai::Tensor result = cppai::add(lhs, rhs);
+
+    EXPECT_EQ(result[0], 6.0);
+    EXPECT_EQ(result[1], 8.0);
+    EXPECT_EQ(result[2], 10.0);
+    EXPECT_EQ(result[3], 12.0);
+}
+
+TEST(TensorOperationsTest, Subtract)
+{
+    cppai::Tensor lhs(
+        {2, 2},
+        {5.0, 6.0, 7.0, 8.0});
+
+    cppai::Tensor rhs(
+        {2, 2},
+        {1.0, 2.0, 3.0, 4.0});
+
+    cppai::Tensor result = cppai::subtract(lhs, rhs);
+
+    EXPECT_EQ(result[0], 4.0);
+    EXPECT_EQ(result[1], 4.0);
+    EXPECT_EQ(result[2], 4.0);
+    EXPECT_EQ(result[3], 4.0);
+}
+
+TEST(TensorOperationsTest, Multiply)
+{
+    cppai::Tensor lhs(
+        {2, 2},
+        {1.0, 2.0, 3.0, 4.0});
+
+    cppai::Tensor rhs(
+        {2, 2},
+        {2.0, 2.0, 2.0, 2.0});
+
+    cppai::Tensor result = cppai::multiply(lhs, rhs);
+
+    EXPECT_EQ(result[0], 2.0);
+    EXPECT_EQ(result[1], 4.0);
+    EXPECT_EQ(result[2], 6.0);
+    EXPECT_EQ(result[3], 8.0);
+}
+
+TEST(TensorOperationsTest, Divide)
+{
+    cppai::Tensor lhs(
+        {2, 2},
+        {2.0, 4.0, 6.0, 8.0});
+
+    cppai::Tensor rhs(
+        {2, 2},
+        {2.0, 2.0, 2.0, 2.0});
+
+    cppai::Tensor result = cppai::divide(lhs, rhs);
+
+    EXPECT_EQ(result[0], 1.0);
+    EXPECT_EQ(result[1], 2.0);
+    EXPECT_EQ(result[2], 3.0);
+    EXPECT_EQ(result[3], 4.0);
 }
