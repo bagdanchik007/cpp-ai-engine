b8e6f76defc49d0177e43910812caf681dc85a3f
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-09 16:31:01 +0200

    test(autograd): extend autograd tests

--- tests/autograd/autograd_tests.cpp
+++ tests/autograd/autograd_tests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <cmath>
 
 #include <cppai/autograd/variable.hpp>
 
@@ -53,4 +54,50 @@ TEST(VariableTest, ReLUZeroesNegativeGradients)
 
     EXPECT_DOUBLE_EQ(a.grad()[0], 0.0);
     EXPECT_DOUBLE_EQ(a.grad()[1], 1.0);
+}
+
+TEST(VariableTest, ExpBackward)
+{
+    Variable a(Tensor({1}, {2.0}), true);
+    Variable c = a.exp().sum();
+    c.backward();
+
+    EXPECT_NEAR(a.grad()[0], std::exp(2.0), 1e-9);
+}
+
+TEST(VariableTest, LogBackward)
+{
+    Variable a(Tensor({1}, {2.0}), true);
+    Variable c = a.log().sum();
+    c.backward();
+
+    EXPECT_NEAR(a.grad()[0], 0.5, 1e-9);
+}
+
+TEST(VariableTest, TanhBackward)
+{
+    Variable a(Tensor({1}, {0.0}), true);
+    Variable c = a.tanh().sum();
+    c.backward();
+
+    // d/dx tanh(x) at x=0 is 1 - tanh(0)^2 = 1.
+    EXPECT_NEAR(a.grad()[0], 1.0, 1e-9);
+}
+
+TEST(VariableTest, SoftmaxSumsToOneAndBackwardShape)
+{
+    Variable a(Tensor({1, 3}, {1.0, 2.0, 3.0}), true);
+    Variable probabilities = a.softmax();
+
+    double total = probabilities.data()[0] + probabilities.data()[1] + probabilities.data()[2];
+    EXPECT_NEAR(total, 1.0, 1e-9);
+
+    Variable loss = probabilities.sum();
+    loss.backward();
+
+    // Gradient of softmax output summed to 1 w.r.t. its own input is
+    // always exactly zero (the Jacobian rows sum to zero).
+    EXPECT_NEAR(a.grad()[0], 0.0, 1e-9);
+    EXPECT_NEAR(a.grad()[1], 0.0, 1e-9);
+    EXPECT_NEAR(a.grad()[2], 0.0, 1e-9);
 }
