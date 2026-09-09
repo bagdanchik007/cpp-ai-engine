ebe6f1fc10af184d6329ccb1bc75b1676e9cea2c
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-09 16:31:10 +0200

    test(nn): extend neural network tests

--- tests/nn/nn_tests.cpp
+++ tests/nn/nn_tests.cpp
@@ -60,3 +60,78 @@ TEST(SoftmaxTest, SumsToOne)
 
     EXPECT_NEAR(total, 1.0, 1e-9);
 }
+
+#include <cppai/nn/activations/gelu.hpp>
+#include <cppai/nn/layers/dropout.hpp>
+#include <cppai/nn/layers/feed_forward.hpp>
+#include <cppai/nn/layers/layer_norm.hpp>
+
+TEST(LayerNormTest, StandardizesEachRow)
+{
+    cppai::nn::LayerNorm ln(4);
+    Variable input(Tensor({2, 4}, {1, 2, 3, 4, 10, 20, 30, 40}), true);
+    Variable output = ln.forward(input);
+
+    for (cppai::size_type row = 0; row < 2; ++row)
+    {
+        double mean = 0.0;
+
+        for (cppai::size_type c = 0; c < 4; ++c)
+        {
+            mean += output.data()[row * 4 + c];
+        }
+
+        mean /= 4.0;
+
+        EXPECT_NEAR(mean, 0.0, 1e-6);
+    }
+}
+
+TEST(LayerNormTest, HasGammaAndBetaParameters)
+{
+    cppai::nn::LayerNorm ln(4);
+    EXPECT_EQ(ln.parameters().size(), 2);
+}
+
+TEST(GELUTest, ZeroAtZero)
+{
+    cppai::nn::GELU gelu;
+    Variable input(Tensor({1}, {0.0}), true);
+    Variable output = gelu.forward(input);
+
+    EXPECT_NEAR(output.data()[0], 0.0, 1e-9);
+}
+
+TEST(GELUTest, ApproachesIdentityForLargePositiveInput)
+{
+    cppai::nn::GELU gelu;
+    Variable input(Tensor({1}, {5.0}), true);
+    Variable output = gelu.forward(input);
+
+    EXPECT_NEAR(output.data()[0], 5.0, 0.01);
+}
+
+TEST(DropoutTest, IdentityWhenNotTraining)
+{
+    cppai::nn::Dropout dropout(0.5);
+    dropout.train(false);
+
+    Variable input(Tensor({4}, {1, 2, 3, 4}), true);
+    Variable output = dropout.forward(input);
+
+    for (cppai::size_type i = 0; i < 4; ++i)
+    {
+        EXPECT_DOUBLE_EQ(output.data()[i], input.data()[i]);
+    }
+}
+
+TEST(FeedForwardTest, OutputShapeMatchesEmbeddingDim)
+{
+    cppai::nn::FeedForward feed_forward(4, 16);
+
+    Variable input(Tensor({1, 4}, {1, 2, 3, 4}));
+    Variable output = feed_forward.forward(input);
+
+    EXPECT_EQ(output.data().shape()[1], 4);
+    EXPECT_EQ(feed_forward.parameters().size(), 4);
+}
