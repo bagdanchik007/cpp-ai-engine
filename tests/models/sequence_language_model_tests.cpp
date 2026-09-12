45647e5baecd7879996bf9e3e608717f367d8138
Bohdan Skibitskyi <bogdanskibitskyi@gmail.com>
2026-09-12 23:34:23 +0200

    test(models): expand sequence language model tests

--- tests/models/sequence_language_model_tests.cpp
+++ tests/models/sequence_language_model_tests.cpp
@@ -1,5 +1,7 @@
 #include <gtest/gtest.h>
 
+#include <cppai/core/error.hpp>
+
 #include <cppai/models/sequence_language_model.hpp>
 #include <cppai/optim/sgd.hpp>
 
@@ -47,6 +49,89 @@ TEST(SequenceLanguageModelTest, TrainingReducesLoss)
     double last_loss = 0.0;
 
     for (int step = 0; step < 30; ++step)
+    {
+        optimizer.zero_grad();
+        auto logits = model.forward_tokens(context);
+
+        Tensor target_onehot(logits.data().shape());
+        target_onehot[target] = 1.0;
+        Variable target_var(target_onehot, false);
+
+        auto diff = logits - target_var;
+        auto loss = (diff * diff).sum();
+        loss.backward();
+        optimizer.step();
+
+        if (step == 0)
+        {
+            first_loss = loss.data()[0];
+        }
+
+        last_loss = loss.data()[0];
+    }
+
+    EXPECT_LT(last_loss, first_loss);
+}
+
+#include <cppai/models/language_model.hpp>
+#include <cppai/models/trainer.hpp>
+#include <cppai/nn/losses/mse_loss.hpp>
+
+TEST(TrainerTest, RunsStepsAndReducesLoss)
+{
+    cppai::models::LanguageModel model(10, 4, 8);
+    cppai::optim::SGD optimizer(model.parameters(), 0.1);
+    cppai::nn::MSELoss loss;
+
+    cppai::models::Trainer trainer(optimizer, loss);
+
+    std::vector<cppai::size_type> context = {1, 2};
+    cppai::size_type target = 3;
+
+    auto result = trainer.run(30, [&](cppai::size_type) {
+        auto logits = model.forward_tokens(context);
+
+        Tensor target_onehot(logits.data().shape());
+        target_onehot[target] = 1.0;
+        Variable target_var(target_onehot, false);
+
+        return std::make_pair(logits, target_var);
+    });
+
+    EXPECT_LT(result.last_loss, result.first_loss);
+}
+
+#include <cppai/models/tiny_transformer.hpp>
+#include <cppai/optim/adamw.hpp>
+
+TEST(TinyTransformerTest, ForwardTokensOutputShape)
+{
+    cppai::models::TinyTransformer model(15, 8, 8, 16);
+    Variable logits = model.forward_tokens({1, 2, 3});
+
+    EXPECT_EQ(logits.data().shape()[0], 1);
+    EXPECT_EQ(logits.data().shape()[1], 15);
+}
+
+TEST(TinyTransformerTest, RejectsMismatchedHeadAndEmbeddingDim)
+{
+    EXPECT_THROW(
+        { cppai::models::TinyTransformer model(15, 8, 4, 16); },
+        cppai::Error);
+}
+
+TEST(TinyTransformerTest, TrainingReducesLoss)
+{
+    cppai::models::TinyTransformer model(15, 8, 8, 16);
+    cppai::optim::AdamW optimizer(model.parameters(), 0.05);
+
+    std::vector<cppai::size_type> context = {1, 2, 3};
+    cppai::size_type target = 4;
+
+    double first_loss = 0.0;
+    double last_loss = 0.0;
+
+    for (int step = 0; step < 40; ++step)
     {
         optimizer.zero_grad();
         auto logits = model.forward_tokens(context);
